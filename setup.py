d26ed028c0eccdfdc94316eaaf07982d8520ee9e
Michael Howitz <mh@gocept.com>
2018-12-01 08:23:50 +0100

    officially support Python 3.7
    https://github.com/httplib2/httplib2/issues/123

--- setup.py
+++ setup.py
@@ -100,6 +100,7 @@ A comprehensive HTTP client library, ``httplib2`` supports many features left ou
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Software Development :: Libraries",
     ),
